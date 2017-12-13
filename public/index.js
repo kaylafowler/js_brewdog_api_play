c6b512e6f033e54dd17723bc940899d012f510ce
SwayGatsby <kayla.fowler@hotmail.com>
2017-12-13 19:15:36 +0000

    iniital commit, can pull back names of all beers

---
+++ public/index.js
@@ -0,0 +1,35 @@
+const app = function () {
+const url = "https://api.punkapi.com/v2/beers";
+
+const button = document.querySelector("#get-beers");
+  button.addEventListener("click", function(){
+    makeRequest(url, requestComplete)
+  });
+};
+
+
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.send();
+  request.addEventListener("load", callback);
+};
+
+
+const requestComplete = function(){
+  if(this.status != 200) return;
+  const jsonString = this.responseText;
+  const beers = JSON.parse(jsonString);
+  populateList(beers);
+};
+
+const populateList = function(beers){
+  const ul = document.querySelector("#beer-list");
+  beers.forEach(function(beer){
+    const li = document.createElement("li");
+    li.innerText = beer.name;
+    ul.appendChild(li);
+  })
+};
+
+document.addEventListener('DOMContentLoaded', app);
